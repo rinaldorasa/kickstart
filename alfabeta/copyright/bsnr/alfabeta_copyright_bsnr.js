1bfc6ea4c8e340f803131b56c69c56a22630aedf
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-01-13 19:22:09 +0100

    Add files via upload

---
+++ alfabeta/copyright/bsnr/alfabeta_copyright_bsnr.js
@@ -0,0 +1,39 @@
+	document.getElementById("alfabeta_copyright_bsnr").innerHTML=
+	' '+
+	' <style> '+
+	' '+
+	' '+
+	' '+
+	' '+
+	' '+
+	' '+
+	' '+
+	' span#copyright_beatsupernova '+
+	' { '+
+	'  padding-left:0.2rem !important;'+
+	'  padding-right:0.2rem !important;'+	
+	'  background-color:rgba(255,170,0,0.5) !important;'+
+	'  color:rgba(0,0,0,0.5) !important;'+	
+	'  font-size:1.1rem !important; '+	
+	'  font-family: "PT Mono", monospace !important; '+
+	'  font-weight:bold !important;'+	
+	'  border-radius:0.2rem !important;'+	
+	' }  '+
+	' '+
+	' '+
+	' '+
+	' '+
+	' '+
+	' </style> '+
+	' '+
+	
+	' <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500&family=Inconsolata:wght@500&family=PT+Mono&family=Roboto+Mono&display=swap" '+
+	' rel="stylesheet">'+
+	
+	' <span id="copyright_beatsupernova">'+
+	' © 1997-2021 beatsupernova '+
+	' </span> '+
+	
+	' '+
+	' '+
+	' ';
