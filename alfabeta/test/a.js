b08dca9e79857b8820677be4cde81c42d0176c7c
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-01-19 08:39:58 +0100

    Add files via upload

---
+++ alfabeta/test/a.js
@@ -0,0 +1,19 @@
+document.getElementById("a").innerHTML=
+
+' <div style="font-size:2rem !important;" > '+
+'  '+
+' à '+
+' ò '+
+' è '+
+' é '+
+' ù '+
+' ì '+
+'  '+
+'  '+
+'  '+
+'  '+
+'  '+
+
+' </div> '+
+
+'';
