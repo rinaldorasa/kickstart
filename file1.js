bafef4dbe7b847242a20e956fa591284498cdbca
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-06-26 10:10:24 +0200

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,18 @@
+/***************************/
+/***********test************/
+
+"use strict";
+let test;
+let oggi= new Date();
+
+
+document.getElementById("oggi").innerHTML=
+"QUELLO CHE VEDI è UN FILE ESTERNO <BR>"+"eccomi sono esterno[file1]"+
+" con aggiunta di variabili"+
+" in maniera da testare il refresh asincrono "+
+" sessione di debugging "+
+"FINE"+oggi+""
+;
+
+
+
